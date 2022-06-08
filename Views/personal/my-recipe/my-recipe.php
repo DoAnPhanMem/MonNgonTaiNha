8f7a4d6c16097670ee01c17e08f3e2159fb1ee94
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2022-06-09 00:18:13 +0700

    code myrecipe

--- Views/personal/my-recipe/my-recipe.php
+++ Views/personal/my-recipe/my-recipe.php
@@ -1,14 +1,40 @@
 <div class="my-recipe">
     <div class="my-recipe__top">
         <h1 class="my-recipe__title">Công Thức Của Tôi</h1>
-        <a class="btn-new" href = "/"> Tạo mới</a>
+        <a class="btn-rounded btn" href = "/"> Tạo mới</a>
     </div>
     <div class="my-recipe__content">
         <div class="my-recipe__fitter">
             <div class ="my-recipe__fitter-search">
-                <input  />
-                
+                <i class="my-recipe__search-icon fa-solid fa-magnifying-glass"></i>
+                <input class = "my-recipe__search-input" placeholder = "Nhập tên công thức" />
             </div>
+            
+                <select class = "my-recipe-fitter__time-update id = "time-update">
+                    <option>Mới nhất</option>
+                    <option>Lâu nhất</option>
+                    <option>1 năm trước</option>
+                </select>
+            
+            
+                <select  class = "my-recipe-fitter__time-cookingid = "time-cooking">
+                    <option>Dưới 30 phút</option>
+                    <option>Trên 30 phút</option>
+                    <option>Trên 1 giờ</option>
+                </select>
+            
+            
+            
+                <select  class = "my-recipe-fitter__theme" id = "theme">
+                    <option>Tất cả</option> 
+                </select>
+            
+        </div>
+        <div class = "my-recipes" >
+                <div class="recipe-item">kkkk</div>
+                <div class="recipe-item">kkkk</div>
+                <div class="recipe-item">kkkk</div>
+                <div class="recipe-item">kkkk</div>
         </div>
     </div>
 </div>
