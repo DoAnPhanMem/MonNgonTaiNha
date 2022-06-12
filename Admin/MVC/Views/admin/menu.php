70f66d9bf2a394d0bdb1adb94931fdf28d978e60
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-06-12 21:43:57 +0700

    Thanh Nhan

--- Admin/MVC/Views/admin/menu.php
+++ Admin/MVC/Views/admin/menu.php
@@ -22,56 +22,62 @@
 
 <!-- Nav Item - Pages Collapse Menu -->
 <?php if (isset($_SESSION['isLogin_Admin']) && $_SESSION['isLogin_Admin'] == true) { ?>
-<li class="nav-item">
-  <a class="nav-link" href="index.php">
-    <i class="fas fa-fw fa-chart-area"></i>
-    <span>Trang chủ</span></a>
-</li>
-<?php } ?>
-<!-- Nav Item - Charts -->
+  <li class="nav-item">
+    <a class="nav-link" href="index.php">
+      <i class="fas fa-fw fa-chart-area"></i>
+      <span>Trang chủ</span></a>
+  </li>
+  <?php } ?>
+  <!-- Nav Item - Charts -->
+  <?php if (isset($_SESSION['isLogin_Admin']) && $_SESSION['isLogin_Admin'] == true) { ?>
+  <li class="nav-item">
+    <a class="nav-link" href="?mod=user">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Quản lý Tài khoản</span></a>
+  </li>
+  <?php } ?>
+  <!-- Nav Item - Tables -->
+  <li class="nav-item">
+    <a class="nav-link" href="?mod=recipe">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Quản lý Công thức</span></a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="?mod=theme">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Quản lý Chủ đề</span></a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="?mod=theme">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Quản lý Bài đăng</span></a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="?mod=statistical">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Thống Kê</span></a>
+  </li>
+
+
+
+
 <?php if (isset($_SESSION['isLogin_Admin']) && $_SESSION['isLogin_Admin'] == true) { ?>
-<li class="nav-item">
-  <a class="nav-link" href="?mod=user">
-    <i class="fas fa-fw fa-table"></i>
-    <span>Quản lý Tài khoản</span></a>
-</li>
-<?php } ?>
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-  <a class="nav-link" href="?mod=recipe">
-    <i class="fas fa-fw fa-table"></i>
-    <span>Quản lý Công thức</span></a>
-</li>
-
-<li class="nav-item">
-  <a class="nav-link" href="?mod=theme">
-    <i class="fas fa-fw fa-table"></i>
-    <span>Quản lý Chủ đề</span></a>
-</li>
-
-<li class="nav-item">
-  <a class="nav-link" href="?mod=statistical">
-    <i class="fas fa-fw fa-table"></i>
-    <span>Thống Kê</span></a>
-</li>
+  <li class="nav-item">
+    <a class="nav-link" href="?mod=banner">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Quản lý Banner</span></a>
+  </li>
+  <?php }?>
 
 
 
-
-<?php if (isset($_SESSION['isLogin_Admin']) && $_SESSION['isLogin_Admin'] == true) { ?>
-<li class="nav-item">
-  <a class="nav-link" href="?mod=banner">
-    <i class="fas fa-fw fa-table"></i>
-    <span>Quản lý Banner</span></a>
-</li>
-<?php }?>
-
-
-
-<!-- Sidebar Toggler (Sidebar) -->
-<div class="text-center d-none d-md-inline">
-  <button class="rounded-circle border-0" id="sidebarToggle"></button>
-</div>
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
 
 </ul>
 <!-- End of Sidebar -->
