bd6ef94377bb1a5078f361cfb8f3886e2dd6f80e
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-06-13 20:01:44 +0700

    Thanh Nhan

--- Admin/MVC/Views/admin/index.php
+++ Admin/MVC/Views/admin/index.php
@@ -138,6 +138,27 @@
                           break;
                       }
                       break;
+                    
+                    case 'post':
+                      switch ($act) {
+                        case 'list':
+                          require_once('MVC/views/post/list.php');
+                          break;
+                        case 'add':
+                          require_once('MVC/views/post/add.php');
+                          break;
+                        case 'detail':
+                          require_once('MVC/views/post/detail.php');
+                          break;
+                        case 'edit':
+                          require_once('MVC/views/post/edit.php');
+                          break;
+                        default:
+                          require_once('MVC/views/post/list.php');
+                          break;
+                      }
+                      break;
+
                     case 'statistical':
                         require_once('MVC/views/statistical/statistical.php');
                         break;
---
+++ Admin/MVC/controllers/PostController.php
@@ -0,0 +1,2 @@
+<?php
+require_once("MVC/Models/post.php");
