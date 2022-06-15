e9e4a9e50afc2cfd8d9042e87b9852c3f4354735
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2022-06-16 00:12:37 +0700

    insert recipe

--- Controllers/MyRecipeController.php
+++ Controllers/MyRecipeController.php
@@ -10,5 +10,75 @@ class MyRecipeController{
     {
         require_once('Views/index.php');
     }
+    public function create_action(){
+
+        $idRecent =  $this->recipe_model->getIdRecent();
+    
+        if((intval($idRecent) + 1) >= 10){
+            $idNew = "BD0";
+        }
+        else{
+            $idNew = "BD00";
+        }
+        $idNew .= ($idRecent + 1);
+       
+        //echo $idNew;
+        if(isset($_POST['post-themes'])){
+            
+            $data_recipe = array(
+                'MaBaiDang' =>$idNew,
+                'MaND' => '1', //tạm,
+                'Video' =>'null',
+                'TieuDe' => $_POST['post-name'],
+                'MoTa' => $_POST['post-description'],
+                'KhauPhan' => $_POST['post-ration'],
+                'TrangThai' =>'Chưa duyệt',
+                'DoKho' => '3', //tạm,
+                'GhiChu' => $_POST['post-note'],
+                'LuotXem' => 'null',
+            );
+            $this->recipe_model->create($data_recipe);
+
+
+            $data_themes = json_decode($_POST['post-themes']) ;
+           /*  echo "themes : "; print_r($data_themes) ;   */
+            $this->recipe_model->create_themeDetail( $data_themes, $idNew );
+            // save imgs to folder 
+            $data_imgs = array();
+            $imgFiles = array_filter($_FILES['post-imgs']['name']); // xóa các item null
+            $imgsLength= count($_FILES['post-imgs']['name']);
+            for($i = 0 ; $i < $imgsLength; $i++){
+                $tmpFilePath = $_FILES['post-imgs']['tmp_name'][$i];
+                if($tmpFilePath != ''){
+                    $newFilePath = "./public/img/recipes/" . $_FILES['post-imgs']['name'][$i];
+                    if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+                        //Other code goes here
+                        $data_imgs[$i] = $_FILES['post-imgs']['name'][$i]; // lưu lại tên hình
+                     }
+                }
+            }
+            $this->recipe_model->create_img($data_imgs, $idNew);
+
+
+            $data_stocks = array();
+           // print_r(json_decode($_POST['post-stocks']));
+            foreach(json_decode($_POST['post-stocks']) as $index => $object){ 
+                foreach ($object as $key => $value) {
+                    $data_stocks[$index][$key] = $value; 
+                }  
+            }
+            $this->recipe_model->create_stock($data_stocks, $idNew);
+
+            print_r($data_stocks);
+            $data_steps =  array();
+            foreach(json_decode( $_POST['post-steps']) as $index => $object){ 
+                foreach ($object as $key => $value) {
+                    $data_steps [$index][$key] = $value; 
+                }  
+            }
+        
+            //header('Location:?act=personal&handle=recipe');
+            require_once('Views/index.php');
+        }
+    }
 }
-?>
--- Views/personal/my-recipe/create.php
+++ Views/personal/my-recipe/create.php
@@ -3,13 +3,12 @@
         <h1 class="create-recipe__title">Thêm Công Thức</h1>
         <hr />
     </div>
-    <form method="POST" action="?act=personal&handle=create-recipe-action">
+    <form method="POST" action="?act=personal&handle=create-action" enctype="multipart/form-data" >
         <div class="create-recipe__form-item">
             <h2>Chủ đề </h2>
             <div class="create-recipe__item-content">
                 <div class="create-recipe__theme-content">
                     <div class="create-recipe__themes-selected">
-                       
                     </div>
                     <?php
                         $themes = array(
@@ -18,7 +17,8 @@
                         ); 
                         ?>
                     <script> 
-                        var themes = <?php echo json_encode($themes) ?>
+                        var themes = <?php echo json_encode($themes) ?>;
+                        
                     </script>
                     <div class="create-recipe__theme-search-group">
                         <i class="fa-solid create-recipe__theme-plus-icon fa-file-circle-plus"></i>
@@ -27,6 +27,7 @@
                     <div class="create-recipe__theme-results">
                         
                     </div>
+                    <input type="hidden" name="post-themes" value="">
                 </div>
 
 
@@ -35,7 +36,7 @@
         <div class="create-recipe__form-item">
             <h2>Tên công thức </h2>
             <div class="create-recipe__item-content">
-                <input  type="text" name="" placeholder="Nhập tên công thức " id="create-recipe__name">  
+                <input  type="text" name="post-name" placeholder="Nhập tên công thức " id="create-recipe__name">  
             </div>
                 
         </div>
@@ -45,7 +46,7 @@
                         <div class="create-recipe__description">
                             <h2>Mô tả </h2>
                             <div class="create-recipe__item-content">
-                                <textarea name="" id="create-recipe__description" cols="30" rows="10">
+                                <textarea name="post-description" id="create-recipe__description" cols="30" rows="10">
 
                                 </textarea>
                             </div>
@@ -56,11 +57,15 @@
                         <div class="create-recipe__video">
                             <h2>Video </h2>
                             <div class="create-recipe__item-content">
-                                <input type="file" name="" id="create-recipe__video-input">
-                                <label  class="create-recipe__video-label" for="create-recipe__video-input">
-                                    <i class="create-recipe__add-icon fa-solid fa-plus"></i>
-                                  
-                                </label>
+                                <input type="file" name="" id="create-recipe__video-input" accept="video/*">
+                                <div class="create-recipe__video-group ">
+                                    <label  class="create-recipe__video-label create-recipe__video-label--change" for="create-recipe__video-input">
+                                        <i class="create-recipe__add-icon fa-solid fa-plus"></i>
+                                        <i class="create-recipe__change-icon fa-solid fa-repeat"></i>
+                                    </label>
+                                    <video controls src="" class="create-recipe__video-tag"></video>
+                                </div>
+                               <input type="hidden" name="post-video">
 
                             </div>
                         </div> 
@@ -69,62 +74,68 @@
                 <div class="create-recipe__images">
                     <h2>Hình ảnh</h2>
                     <div class="create-recipe__item-content">
-                        <input type="file"  accept="image/png, image/jpeg" multiple name="" id="create-recipe__image-input">
+                        <input type="file"  accept="image/png, image/jpeg" multiple name="post-imgs[]" id="create-recipe__image-input">
                         <label  class="create-recipe__image-label" for="create-recipe__image-input">
                             <i  class=" create-recipe__add-icon fa-solid fa-plus"></i>
                             <!-- <img src="./public/img/recipes/1.jpg" alt=""> -->
                         </label>
                     </div>
-                    <div class="create-recipe__images-preview">
-                        <i>Chưa có ảnh nào được chọn</i>
+                    <div class="create-recipe__images-preview ">
+                         <p><i>Chưa có ảnh nào được chọn</i></p>
+                        <label  class="create-recipe__image-label--add-more" for="create-recipe__image-input">
+                            <i  class=" create-recipe__add-icon fa-solid fa-plus"></i>
+                        </label>
                     </div>
-        
+                       
                 </div>        
         </div>
         
         <div class="create-recipe__form-item">
             <h2>Thành phần </h2>
             <div class="create-recipe__stocks">
-                <div class="create-recipe__stock">
-                    <input  type="text" name=""  placeholder="Tên nguyên liệu " id="create-recipe__stock-name">  
-                    <input  type="number" name="" placeholder="Số lượng " id="create-recipe__stock-quantity">  
-                    <input  type="text" name="" placeholder="Đơn vị " id="create-recipe__stock-unit">  
-                    <span class="create-recipe__stock-btn-delete">
-                        <i class="fa-solid fa-circle-minus"></i>
-                    </span>
+                <div class="create-recipe__stock-list">
+                    
                 </div>
+                
                 <span class="create-recipe__stock-btn-add" >
                     <i class="create-recipe__add-icon create-recipe__stock-add-icon fa-solid fa-plus"></i>
                     Thêm thành phần
                 </span>
+                <input type="hidden" name="post-stocks">
             </div>
         </div>
         <div class="create-recipe__form-item">
             <h2>Công thức </h2>
-            <div class="create-recipe__stocks">
-                <div class="create-recipe__stock">
-                    <input  type="text" name=""  placeholder="Nội dung  " id="create-recipe__step-name">  
-                    <input  type="number" name="" placeholder="Thời gian " id="create-recipe__step-time">  
-                    <input  type="text" name="" placeholder="Đơn vị thời gian " id="create-recipe__step-unit">  
-                    <span class="create-recipe__stock-btn-delete">
-                        <i class="fa-solid fa-circle-minus"></i>
-                    </span>
+            <div class="create-recipe__steps">
+                <div class="create-recipe__step-list">
+                    
                 </div>
-                <span class="create-recipe__stock-btn-add" >
+                
+                <span class="create-recipe__step-btn-add" >
                     <i class="create-recipe__add-icon create-recipe__stock-add-icon fa-solid fa-plus"></i>
                     Thêm bước làm
                 </span>
+                <input type="hidden" name="post-steps">
             </div>
         </div>
         <div class="create-recipe__form-item">
             <h2>Khẩu phần </h2>
-            <input  type="number" name=""  placeholder="Khẩu phần " id="create-recipe__ration">  
+            <input  type="number" name="post-ration"  placeholder="Khẩu phần " id="create-recipe__ration">  
 
         </div>
         <div class="create-recipe__form-item">
             <h2>Ghi chú</h2>
-            <input type="text" name="" id="create-recipe__note">
+            <input type="text" name="post-note" id="create-recipe__note">
         </div>
         <button class = "btn btn-rounded create-recipe__btn-submit">Hoàn tất</button>
     </form>
 </div>
+<script src="public/js/create-recipe.js"></script>
+
+INSERT INTO yeuthich (MaND, MaBaiDang) VALUES
+(1, 'BD001'),
+(2, 'BD002'),
+(3, 'BD003'),
+(4, 'BD004'),
+(5, 'BD005'),
+(6, 'BD006');
