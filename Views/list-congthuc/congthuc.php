fa6fa806332afd70cde079594ec76e6f362993de
thuthuyxh1311 <thuthuyxh1311@gmail.com>
2022-06-22 20:23:21 +0700

    sua link detail

--- Views/list-congthuc/congthuc.php
+++ Views/list-congthuc/congthuc.php
@@ -5,7 +5,9 @@
             <div class="cart">
                 <img width="100%" height="150px" style="object-fit: cover;" src="public/img/recipes/<?=$product['HinhAnh']?>" alt="">
                 <div class="content">
-                    <p class="title"><?=$product['TieuDe']?></p>
+                    <p class="title">
+                    <a href="?act=detail&id=<?=$product['MaBaiDang']?>" ><?=$product['TieuDe']?></a>
+                    </p>
                     <p class="post-by">Đăng bởi: <?=$product['hoTen']?></p>
                 </div>
             </div>
